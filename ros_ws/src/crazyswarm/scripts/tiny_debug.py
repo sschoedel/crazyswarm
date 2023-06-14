ec54ae26e082c57983640ab82dbfbf189bde3270
Sam Schoedel <seschoedel@gmail.com>
2023-06-14 13:59:50 -0400

    up

--- ros_ws/src/crazyswarm/scripts/tiny_debug.py
+++ ros_ws/src/crazyswarm/scripts/tiny_debug.py
@@ -6,21 +6,23 @@ timeHelper = swarm.timeHelper
 allcfs = swarm.allcfs
 cf = swarm.allcfs.crazyflies[0]
 cf.initialPosition = cf.position()
-Z = 0.5
+Z = 0.4
 
 print("Switching controller")
-cf.setParam("stabilizer/controller", 1)  # MPC
-timeHelper.sleep(0.1)
+cf.setParam("stabilizer/controller", 1)  # 1: PID, 5: MPC
+cf.takeoff(Z, 2)
+timeHelper.sleep(2.0)
 
-for k in range(20):
+
+for k in range(50):
     pos = np.array(cf.initialPosition) + np.array([0, 0, Z])
     cf.cmdPosition(pos)
     timeHelper.sleep(0.1)
 
-for k in range(70):
-    pos = np.array(cf.initialPosition) + np.array([0, 0, Z])
-    cf.cmdPosition(pos)
-    timeHelper.sleep(0.1)
+cf.land(0.05, 2.0)
+timeHelper.sleep(2.0)
+
 
 # turn-off motors
 cf.cmdStop()
+
--- ros_ws/src/crazyswarm/scripts/tiny_individual_hover.py
+++ ros_ws/src/crazyswarm/scripts/tiny_individual_hover.py
@@ -4,28 +4,35 @@ from __future__ import print_function
 
 from pycrazyswarm import *
 
+CONTROLLER_NUM = 5
+
 def main():
     swarm = Crazyswarm()
     timeHelper = swarm.timeHelper
     allcfs = swarm.allcfs
-
     cf = allcfs.crazyflies[0]
+
+    cf.setParam("stabilizer/controller", CONTROLLER_NUM) # 1: PID, 2: mellinger, 5: MPC
     print(cf.id)
-    cf.takeoff(0.5, 2.5)
+    cf.takeoff(0.5, 2.0)
 
     print("press button to continue")
     swarm.input.waitUntilButtonPressed()
     
     print("Switching to controller 5")
-    cf.setParam("stabilizer/controller", 5) # 1: PID, 2: mellinger, 5: MPC
+    cf.setParam("stabilizer/controller", CONTROLLER_NUM) # 1: PID, 2: mellinger, 5: MPC
 
     print("press button to continue")
     swarm.input.waitUntilButtonPressed()
 
     print("Switching to controller 1")
     cf.setParam("stabilizer/controller", 1) # 1: PID, 2: mellinger, 5: MPC
-    cf.land(0.05, 2.0)
+    # timeHelper.sleep(0.5)
+    # cf.setParam("stabilizer/controller", CONTROLLER_NUM) # 1: PID, 2: mellinger, 5: MPC
+    cf.land(0.05, 2.5)
+    timeHelper.sleep(2.5)
 
+    cf.cmdStop()
 
 if __name__ == "__main__":
     main()
