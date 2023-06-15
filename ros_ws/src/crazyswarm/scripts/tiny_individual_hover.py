93be13cad7091bf2650fe174f5dbfb91b8bc4177
Sam Schoedel <seschoedel@gmail.com>
2023-06-15 14:42:59 -0400

    up

--- ros_ws/src/crazyswarm/scripts/tiny_individual_hover.py
+++ ros_ws/src/crazyswarm/scripts/tiny_individual_hover.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 
 from pycrazyswarm import *
 
-CONTROLLER_NUM = 5
 
 def main():
     swarm = Crazyswarm()
@@ -12,24 +11,25 @@ def main():
     allcfs = swarm.allcfs
     cf = allcfs.crazyflies[0]
 
-    cf.setParam("stabilizer/controller", CONTROLLER_NUM) # 1: PID, 2: mellinger, 5: MPC
-    print(cf.id)
-    cf.takeoff(0.5, 2.0)
+    cf.setParam("stabilizer/controller", 1)
+    print(f"crazyflie id: {cf.id}")
+    cf.takeoff(0.25, 2.0)
+    timeHelper.sleep(2.0)
 
-    print("press button to continue")
+    # print("Select controller to switch to: 1: PID, 2: mellinger, 3: INDI, 4: Brescianini, 5: TinyMPC")
+    # button = swarm.input.waitUntilAnyButtonPressed()
+    print("press any button to continue")
     swarm.input.waitUntilButtonPressed()
     
-    print("Switching to controller 5")
-    cf.setParam("stabilizer/controller", CONTROLLER_NUM) # 1: PID, 2: mellinger, 5: MPC
+    print("Switching controller")
+    cf.setParam("stabilizer/controller", 5) # 1: PID, 4: Brescianini, 5: TinyMPC
 
-    print("press button to continue")
+    print("press any button to land")
     swarm.input.waitUntilButtonPressed()
 
     print("Switching to controller 1")
-    cf.setParam("stabilizer/controller", 1) # 1: PID, 2: mellinger, 5: MPC
-    # timeHelper.sleep(0.5)
-    # cf.setParam("stabilizer/controller", CONTROLLER_NUM) # 1: PID, 2: mellinger, 5: MPC
-    cf.land(0.05, 2.5)
+    cf.setParam("stabilizer/controller", 1)
+    cf.land(0.02, 2.5)
     timeHelper.sleep(2.5)
 
     cf.cmdStop()
