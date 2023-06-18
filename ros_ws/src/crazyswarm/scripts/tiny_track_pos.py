b6e87784f915286fdfafb63b91bd0c117b811e45
xkhainguyen <nxk120199@gmail.com>
2023-06-18 15:59:25 -0400

    up

--- ros_ws/src/crazyswarm/scripts/tiny_track_pos.py
+++ ros_ws/src/crazyswarm/scripts/tiny_track_pos.py
@@ -4,17 +4,23 @@ from __future__ import print_function
 
 from pycrazyswarm import *
 
+frequency = 500
+uHover = 0.67
+trajLength = 4495
+trajHold = 1  # 1 is most agressive
 
 def main():
     swarm = Crazyswarm()
     timeHelper = swarm.timeHelper
     allcfs = swarm.allcfs
     cf = allcfs.crazyflies[0]
+    print(f"crazyflie id: {cf.id}")
 
     cf.setParam("stabilizer/controller", 1)
-    print(f"crazyflie id: {cf.id}")
-    cf.takeoff(0.5, 2.0)
     timeHelper.sleep(2.0)
+
+    cf.takeoff(1.0, 3.0)
+    timeHelper.sleep(3.0)
 
     # print("Select controller to switch to: 1: PID, 2: mellinger, 3: INDI, 4: Brescianini, 5: TinyMPC")
     # button = swarm.input.waitUntilAnyButtonPressed()
@@ -23,7 +29,10 @@ def main():
     
     print("Switching controller")
     cf.setParam("stabilizer/controller", 5) # 1: PID, 4: Brescianini, 5: TinyMPC
-    timeHelper.sleep(8.0)
+    cf.setParam("ctrlMPC/trajHold", trajHold)
+    cf.setParam("ctrlMPC/trajLength", trajLength)
+    cf.setParam("ctrlMPC/uHover", uHover)
+    timeHelper.sleep(trajLength*trajHold/500 + 1)
 
     # print("press any button to land")
     # swarm.input.waitUntilButtonPressed()
