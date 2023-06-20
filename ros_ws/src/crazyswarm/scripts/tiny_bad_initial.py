0350cee39b522f577ef499ee0318eac533e7c942
xkhainguyen <nxk120199@gmail.com>
2023-06-20 15:30:26 -0400

    up

--- ros_ws/src/crazyswarm/scripts/tiny_bad_initial.py
+++ ros_ws/src/crazyswarm/scripts/tiny_bad_initial.py
@@ -6,9 +6,6 @@ from pycrazyswarm import *
 
 frequency = 500     # control frequency
 uHover = 0.67       # hovering offset
-trajLength = 1000    # for delay time 
-trajIter = 1        # number of laps
-trajHold = 2        # 1 is most agressive
 
 def main():
     swarm = Crazyswarm()
@@ -18,34 +15,30 @@ def main():
     print(f"crazyflie id: {cf.id}")
 
     cf.setParam("stabilizer/controller", 1)
-    timeHelper.sleep(1.0)
+    # timeHelper.sleep(1.0)
 
-    cf.takeoff(1.0, 2.0)  
-    timeHelper.sleep(2.0)
+    # cf.cmdPosition([0, 0, 1])
+    cf.goTo([0, 0, 1], 0, 3)
+    # timeHelper.sleep(1.0)
 
     # print("Select controller to switch to: 1: PID, 2: mellinger, 3: INDI, 4: Brescianini, 5: TinyMPC")
     # button = swarm.input.waitUntilAnyButtonPressed()
     # print("press any button to continue")
     # swarm.input.waitUntilButtonPressed()
     
-    print("Switching controller")
-    cf.setParam("stabilizer/controller", 5) # 1: PID, 4: Brescianini, 5: TinyMPC
-    cf.setParam("ctrlMPC/trajHold", trajHold)
-    cf.setParam("ctrlMPC/trajIter", trajIter)
-    # cf.setParam("ctrlMPC/trajLength", trajLength)
+    print("Switching controller")  # Controller 5 doesn't care about cmd
+    cf.setParam("stabilizer/controller", 2) # 1: PID, 4: Brescianini, 5: TinyMPC
     cf.setParam("ctrlMPC/uHover", uHover)
-    timeHelper.sleep(trajIter*trajLength*trajHold/500 + 1)
+    timeHelper.sleep(5)
 
     # print("press any button to land")
     # swarm.input.waitUntilButtonPressed()
 
     print("Switching to controller 1")
     cf.setParam("stabilizer/controller", 1)
-
-    cf.land(0.02, 2.5)
-    timeHelper.sleep(2.5)
-
-    cf.cmdStop()
+    # cf.goTo([0, 0, 0.2], 0, 3)
+    cf.land(0.02, 2)
+    timeHelper.sleep(2)
 
 if __name__ == "__main__":
     main()
