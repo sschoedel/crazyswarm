59b25a464f6a5c5815962155fffdc9af2de9c897
Sam Schoedel <seschoedel@gmail.com>
2023-09-05 21:18:35 -0400

    update

--- ros_ws/src/crazyswarm/scripts/tiny_hover_obs.py
+++ ros_ws/src/crazyswarm/scripts/tiny_hover_obs.py
@@ -11,7 +11,7 @@ def main():
     swarm = Crazyswarm()
     timeHelper = swarm.timeHelper
     allcfs = swarm.allcfs
-    rat = allcfs.crazyflies[1]  # avoid the cat
+    rat = allcfs.crazyflies[0]  # avoid the obstacle
 
     timeHelper.sleep(0.5)
 
@@ -19,40 +19,51 @@ def main():
     print(f"crazyflie id: {rat.id}")
     rat.takeoff(1.0, 2.0)
     timeHelper.sleep(2.0)
-    rat.goTo([0, 0, 1], 0, 3.0)
-    timeHelper.sleep(1.5)
+    # rat.goTo([0, 0, 1], 0, 3.0)
+    # timeHelper.sleep(1.5)
 
-    print("press any button to run MPC")
+    # print("press any button to run MPC")
 
-    with keyboard.KeyPoller() as keyPoller:
-        # Wait until a key is pressed.
-        while keyPoller.poll() is None:
-            timeHelper.sleep(0.01)
-        # Wait until the key is released.
-        while keyPoller.poll() is not None:
-            timeHelper.sleep(0.01)
+    # with keyboard.KeyPoller() as keyPoller:
+    #     # Wait until a key is pressed.
+    #     while keyPoller.poll() is None:
+    #         timeHelper.sleep(0.01)
+    #     # Wait until the key is released.
+    #     while keyPoller.poll() is not None:
+    #         timeHelper.sleep(0.01)
 
     print("Switching to TinyMPC")
-
     rat.setParam("stabilizer/controller", 5) # 1: PID, 4: Brescianini, 5: TinyMPC
-    rat.goTo([0, 0, 0], 0, 0.001) # Move obstacle out of the way
+    # rat.goTo([0, 0, 0], 0, 0.001) # Move obstacle out of the way
 
-    print("press any button to land")
+    # print("press any button to land")
+    for i in range(30):
+        rat_pos = rat.position() - [0, 0, 0.4]
+        rat.cmdPosition(rat_pos, yaw=0)
+        timeHelper.sleep(0.1)
 
-    with keyboard.KeyPoller() as keyPoller:
-        # Wait until a key is pressed. Send obstacle pose as a setpoint in the meantime.
-        while keyPoller.poll() is None:
-            # 1. get new obstacle transform from mocap
-            # 2. convert obstacle transform to xyz coords
-            # 3. send transform as a setpoint with cf.goTo
-            rat.goTo([0, 0.1, 0.9], 0, 0.001)
-            rat_pos = rat.position()
-        # Wait until the key is released.
-        while keyPoller.poll() is not None:
-            timeHelper.sleep(0.01)
-
+    # # obs_pos = [0.3, 0, 0.4]
+    # with keyboard.KeyPoller() as keyPoller:
+    #     # Wait until a key is pressed. Send obstacle pose as a setpoint in the meantime.
+    #     while keyPoller.poll() is None:
+    #         # 1. get new obstacle transform from mocap
+    #         # 2. convert obstacle transform to xyz coords
+    #         # 3. send transform as a setpoint with cf.goTo
+    #         rat_pos = rat.position() - [0, 0, 0.4]
+    #         # rat.goTo(obs_pos, 0, 0.5)
+    #         rat.cmdPosition(rat_pos, yaw=0)
+    #         timeHelper.sleep(0.1)            
+    #     # Wait until the key is released.
+    #     while keyPoller.poll() is not None:
+    #         # rat_pos = rat.position() - [0.5, 0, 0.4]
+    #         rat.cmdPosition(rat_pos, yaw=0)
+    #         timeHelper.sleep(1.0)
+    
+    
     print("Switching to controller 1")
-    rat.setParam("stabilizer/controller", 1)
+    timeHelper.sleep(0.1)
+    rat.setParam("stabilizer/controller", 1 )
+    timeHelper.sleep(2.0)
     rat.land(0.02, 2.5)
     timeHelper.sleep(2.5)
 
