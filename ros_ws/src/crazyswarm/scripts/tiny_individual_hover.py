6994eaa6f67d32887f4b3d961d853e67e310b72f
xkhainguyen <nxk120199@gmail.com>
2023-09-05 21:11:56 -0400

    up

--- ros_ws/src/crazyswarm/scripts/tiny_individual_hover.py
+++ ros_ws/src/crazyswarm/scripts/tiny_individual_hover.py
@@ -19,7 +19,7 @@ def main():
     print(f"crazyflie id: {cf.id}")
     cf.takeoff(1.0, 2.0)
     timeHelper.sleep(2.0)
-    cf.goTo([0, 0, 1], 0, 3.0)
+    # cf.goTo([0, 0, 1], 0, 3.0)
     timeHelper.sleep(1.5)
 
     print("press any button to run MPC")
@@ -38,19 +38,19 @@ def main():
 
     print("press any button to land")
 
-    with keyboard.KeyPoller() as keyPoller:
+    # with keyboard.KeyPoller() as keyPoller:
         # Wait until a key is pressed. Send obstacle pose as a setpoint in the meantime.
-        while keyPoller.poll() is None:
+        # while keyPoller.poll() is None:
             # 1. get new obstacle transform from mocap
             # 2. convert obstacle transform to xyz coords
             # 3. send transform as a setpoint with cf.goTo
-            cf.goTo([1.5, .5, 1.5], 0, 0.001)
+            # cf.goTo([1.5, .5, 1.5], 0, 0.001)
         # Wait until the key is released.
-        while keyPoller.poll() is not None:
-            timeHelper.sleep(0.01)
+        # while keyPoller.poll() is not None:
+            # timeHelper.sleep(0.01)
     
-    cf.goTo([0.0, 0.0, 1], 0, 2.0)
-    timeHelper.sleep(2.0)
+    # cf.goTo([0.0, 0.0, 1], 0, 2.0)
+    timeHelper.sleep(5.0)
 
     print("Switching to controller 1")
     cf.setParam("stabilizer/controller", 1)
