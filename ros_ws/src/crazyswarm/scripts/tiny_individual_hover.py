2191ac12815040f47a48ac6b0aba209be37033a0
xkhainguyen <nxk120199@gmail.com>
2024-03-10 18:44:14 -0400

    up

--- ros_ws/src/crazyswarm/scripts/tiny_individual_hover.py
+++ ros_ws/src/crazyswarm/scripts/tiny_individual_hover.py
@@ -20,17 +20,19 @@ def main():
     cf.takeoff(0.5, 2.0)
     timeHelper.sleep(2.0)
     # cf.goTo([0, 0, 1], 0, 3.0)
-    timeHelper.sleep(1.5)
+    timeHelper.sleep(1.0)
 
     print("press any button to run MPC")
 
-    with keyboard.KeyPoller() as keyPoller:
-        # Wait until a key is pressed.
-        while keyPoller.poll() is None:
-            timeHelper.sleep(0.01)
-        # Wait until the key is released.
-        while keyPoller.poll() is not None:
-            timeHelper.sleep(0.01)
+    # with keyboard.KeyPoller() as keyPoller:
+    #     # Wait until a key is pressed.
+    #     while keyPoller.poll() is None:
+    #         timeHelper.sleep(0.01)
+    #     # Wait until the key is released.
+    #     while keyPoller.poll() is not None:
+    #         timeHelper.sleep(0.01)
+
+    # timeHelper.sleep(1.0)
 
     print("Switching to TinyMPC")
 
@@ -38,21 +40,21 @@ def main():
 
     print("press any button to land")
 
-    with keyboard.KeyPoller() as keyPoller:
-        # Wait until a key is pressed.
-        while keyPoller.poll() is None:
-            timeHelper.sleep(0.01)
-        # Wait until the key is released.
-        while keyPoller.poll() is not None:
-            timeHelper.sleep(0.01)
+    # with keyboard.KeyPoller() as keyPoller:
+    #     # Wait until a key is pressed.
+    #     while keyPoller.poll() is None:
+    #         timeHelper.sleep(0.01)
+    #     # Wait until the key is released.
+    #     while keyPoller.poll() is not None:
+    #         timeHelper.sleep(0.01)
     
     # cf.goTo([0, 0, 1], 0, 3.0)
-    # timeHelper.sleep(2.0)
+    timeHelper.sleep(4.0)
 
     print("Switching to controller 1")
     cf.setParam("stabilizer/controller", 1)
     timeHelper.sleep(0.1)
-    cf.cmdPosition([0, 0, 0.5], yaw=0)
+    # cf.cmdPosition([0.8, 0, 0.5], yaw=0)
     timeHelper.sleep(1.0)
     cf.land(0.02, 2.5)
     timeHelper.sleep(2.5)
