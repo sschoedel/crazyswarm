bcb61ddebaa3b618da38a48e95bf3baba267b38b
xkhainguyen <nxk120199@gmail.com>
2024-03-17 12:29:05 -0400

    led ring control

---
+++ ros_ws/src/crazyswarm/scripts/tiny_led_ring.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+from pycrazyswarm import *
+
+from pycrazyswarm import keyboard
+
+
+def main():
+    swarm = Crazyswarm()
+    timeHelper = swarm.timeHelper
+    allcfs = swarm.allcfs
+    cf = allcfs.crazyflies[0]
+
+    timeHelper.sleep(0.5)
+
+    off = 0b10000000
+    cf.setParam('led/bitmask', off)
+    cf.setParam('ring/effect', 6)
+    cf.setParam('ring/solidRed', 1)
+    cf.setParam('ring/solidGreen', 0)
+    cf.setParam('ring/solidBlue', 0)
+
+    timeHelper.sleep(2.0)
+
+    cf.setParam('ring/effect', 0)    
+
+    # # Create a spinning effect using the onboard LEDs
+    # for i in range(10):         
+    #     for j in range(8):
+    #         # Set black color effect
+    #         cf.setParam('led/bitmask', bitSeq[j])
+    #         timeHelper.sleep(0.15)
+
+if __name__ == "__main__":
+    main()
